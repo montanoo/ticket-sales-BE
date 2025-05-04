0b291b6a4a2988e3734118f6acc8ae48455b37e3
Fernando Montano <fernando.montano@internetbrands.com>
2025-05-05 00:36:40 +0200

    feat: added basic auth setup for users

---
+++ TicketSystemAPI/TicketSystemAPI/DTO/UserAuthDTO.cs
@@ -0,0 +1,16 @@
+namespace TicketSystemAPI.DTOs
+{
+    public class RegisterDto
+{
+    public string Name { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string Password { get; set; } = null!;
+}
+
+public class LoginDto
+{
+    public string Email { get; set; } = null!;
+    public string Password { get; set; } = null!;
+}
+
+}
---
+++ TicketSystemAPI/TicketSystemAPI/Migrations/20250504193440_IncreasePasswordLength.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TicketSystemAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class IncreasePasswordLength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "password",
+                table: "users",
+                type: "varchar(2000)",
+                maxLength: 2000,
+                nullable: false,
+                collation: "utf8mb3_general_ci",
+                oldClrType: typeof(string),
+                oldType: "varchar(45)",
+                oldMaxLength: 45)
+                .Annotation("MySql:CharSet", "utf8mb3")
+                .OldAnnotation("MySql:CharSet", "utf8mb3")
+                .OldAnnotation("Relational:Collation", "utf8mb3_general_ci");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "basePrice",
+                table: "tickettypes",
+                type: "decimal(10)",
+                precision: 10,
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(10,30)",
+                oldPrecision: 10,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "price",
+                table: "tickets",
+                type: "decimal(10)",
+                precision: 10,
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(10,30)",
+                oldPrecision: 10,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "amount",
+                table: "payments",
+                type: "decimal(10)",
+                precision: 10,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(10,30)",
+                oldPrecision: 10);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "password",
+                table: "users",
+                type: "varchar(45)",
+                maxLength: 45,
+                nullable: false,
+                collation: "utf8mb3_general_ci",
+                oldClrType: typeof(string),
+                oldType: "varchar(2000)",
+                oldMaxLength: 2000)
+                .Annotation("MySql:CharSet", "utf8mb3")
+                .OldAnnotation("MySql:CharSet", "utf8mb3")
+                .OldAnnotation("Relational:Collation", "utf8mb3_general_ci");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "basePrice",
+                table: "tickettypes",
+                type: "decimal(10,30)",
+                precision: 10,
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(10)",
+                oldPrecision: 10,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "price",
+                table: "tickets",
+                type: "decimal(10,30)",
+                precision: 10,
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(10)",
+                oldPrecision: 10,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "amount",
+                table: "payments",
+                type: "decimal(10,30)",
+                precision: 10,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(10)",
+                oldPrecision: 10);
+        }
+    }
+}
