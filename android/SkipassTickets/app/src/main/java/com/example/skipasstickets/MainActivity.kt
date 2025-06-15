88ec860566ea837ae02fd2acf68295f21df91834
itsjustforschool2 <ilikemygames@tutamail.com>
2025-06-15 21:23:17 +0200

    Added a login screen to the QR code generator

--- android/SkipassTickets/app/src/main/java/com/example/skipasstickets/MainActivity.kt
+++ android/SkipassTickets/app/src/main/java/com/example/skipasstickets/MainActivity.kt
@@ -1,27 +1,42 @@
 package com.example.skipasstickets
 
+import android.content.Intent
 import android.os.Bundle
-import android.util.DisplayMetrics
+import android.widget.Toast
 import androidmads.library.qrgenearator.QRGContents
 import androidmads.library.qrgenearator.QRGEncoder
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asImageBitmap
 import com.example.skipasstickets.ui.theme.SkipassTicketsTheme
 
+
 class MainActivity : ComponentActivity() {
+    var userId: Int? = null
+    val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {result ->
+        if (result.resultCode == RESULT_OK)
+        {
+            userId = result.data?.getIntExtra("userId", -1)
+            Toast.makeText(this, userId.toString(), Toast.LENGTH_LONG).show()
+            val qrEncoder = QRGEncoder(userId.toString(), QRGContents.Type.TEXT, 500)
+            setContent {
+                QRCode(qrEncoder.getBitmap(0).asImageBitmap())
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val qrEncoder: QRGEncoder = QRGEncoder("I'd just like to interject for a moment. What you're referring to as Linux, is in fact, GNU/Linux, or as I've recently taken to calling it, GNU plus Linux. Linux is not an operating system unto itself, but rather another free component of a fully functioning GNU system made useful by the GNU corelibs, shell utilities and vital system components comprising a full OS as defined by POSIX.\n" +
-                "\n" +
-                "Many computer users run a modified version of the GNU system every day, without realizing it. Through a peculiar turn of events, the version of GNU which is widely used today is often called Linux, and many of its users are not aware that it is basically the GNU system, developed by the GNU Project.\n" +
-                "\n" +
-                "There really is a Linux, and these people are using it, but it is just a part of the system they use. Linux is the kernel: the program in the system that allocates the machine's resources to the other programs that you run. The kernel is an essential part of an operating system, but useless by itself; it can only function in the context of a complete operating system. Linux is normally used in combination with the GNU operating system: the whole system is basically GNU with Linux added, or GNU/Linux. All the so-called Linux distributions are really distributions of GNU/Linux!\n", QRGContents.Type.TEXT, 500)
+        val i: Intent = Intent(applicationContext, LoginActivity::class.java)
+        launcher.launch(i)
+
+        var qrEncoder: QRGEncoder = QRGEncoder("there is an error in the QR code generator.", QRGContents.Type.TEXT, 500)
 
         enableEdgeToEdge()
         setContent {
